4a391058d41e134080e4c15fcab3152cfb7ba4b8
benblogging <85029125+benblogging@users.noreply.github.com>
2023-04-26 20:33:20 +0700

    Update getterbiaya.js

--- getterbiaya.js
+++ getterbiaya.js
@@ -1,1 +1,1 @@
-function gettoldata(f,r,g,m,k){const tol=getjsonfile(`https://bennypadang.com/wp-content/uploads/tol_${f}.json`);console.log(`awdwadawa - ${tol[r]}`);let type=/*tol[r]["type"]*/"tutup";let t=(type==="tutup")?tol[r][m]['tarif'][k][g]:tol[r]['tarif'][g];return t;}
+async function gettoldata(f,r,g,m,k){const tol=await getjsonfile(`https://raw.githubusercontent.com/benblogging/data-biaya/main/tol/tol_${f}.json`);console.log(tol);console.log(`awdwadawa - ${tol[r]}`);let type=tol[r]["type"];let t=(type==="tutup")?tol[r][m][k][g]:tol[r][g];return t;}
